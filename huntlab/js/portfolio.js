9121097042f6b4211723f5c8b9badad0e5b19a44
daeheekim <daeheekim@nts-corp.com>
2022-04-17 22:51:36 +0900

    포폴페이지 init commit

---
+++ huntlab/js/portfolio.js
@@ -0,0 +1,11 @@
+var gnbBtnHome = document.getElementById("_gnb_btn_home")
+var gnbBtnAbout = document.getElementById("_gnb_btn_about")
+var gnbBtnPortfolio = document.getElementById("_gnb_btn_portfolio")
+var gnbBtnBlog = document.getElementById("_gnb_btn_blog")
+var gnbBtnContact = document.getElementById("_gnb_btn_contact")
+
+gnbBtnHome.onclick = function() { location.href = "#_home" }
+gnbBtnAbout.onclick = function() { location.href = "#_about" }
+gnbBtnPortfolio.onclick = function() { location.href = "#_portfolio" }
+gnbBtnBlog.onclick = function() { location.href = "#_blog" }
+gnbBtnContact.onclick = function() { location.href = "#_contact" }
---
+++ huntlab/portfolio/index.php
@@ -0,0 +1,41 @@
+<?php
+
+?>
+
+<html>
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/portfolio.css?00"/>
+</head>
+<body>
+    <header>
+        <nav>
+            <span class="gnb_btn" id="_gnb_btn_home">Home</span>
+            <span class="gnb_btn" id="_gnb_btn_about">About</span>
+            <span class="gnb_btn" id="_gnb_btn_portfolio">Portfolio</span>
+            <span class="gnb_btn" id="_gnb_btn_blog">Blog</span>
+            <span class="gnb_btn" id="_gnb_btn_contact">Contact</span>
+        </nav>
+    </header>
+
+    <div class="wrap">
+        <div class="content home" id="_home">
+            <img src="../image/unsplash.jpg"/>
+        </div>
+        <div class="content about" id="_about">
+            About
+        </div>
+        <div class="content portfolio" id="_portfolio">
+            Portfolio
+        </div>
+        <div class="content blog" id="_blog">
+            Blog
+        </div>
+        <div class="content contact" id="_contact">
+            Contact
+        </div>
+    </div>
+
+    <script type="text/javascript" src="../js/portfolio.js?00"></script>
+</body>
+</html>
